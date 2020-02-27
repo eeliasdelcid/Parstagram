21d3da755eade5ab887c5439f4c6c1759468008c
Ervin Elias <ervinjr011@gmail.com>
2020-02-27 12:10:35 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Ervin Del Cid on 2/27/20.
+//  Copyright © 2020 Ervin Del Cid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
